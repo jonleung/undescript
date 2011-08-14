c9014ad1016dd3cb56360cda9e5e8bd8f3ad881d
Andrew Braunstein <awbraunstein@gmail.com>
2011-08-14 09:44:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slidefeed::Application.config.secret_token = '6613160f8d426c90209236ef9d05f954630fb85548c66cf16f7b8e062bafbc79fe9e773a513443496c9d681d7ed2239fa67a3a444bd2572140cf41d3735e88b3'
