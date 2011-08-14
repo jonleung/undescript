e7305c38d27941a0dca33d67143358de642ae04e
Daniel Dyba <daniel.dyba@gmail.com>
2011-08-14 16:01:00 -0700

    Added video_url helper

--- app/helpers/video_slides_helper.rb
+++ app/helpers/video_slides_helper.rb
@@ -1,2 +1,6 @@
 module VideoSlidesHelper
+  def video_url(url)
+    youtube_id = /v=(.*)&/.match(url)
+    "<iframe width='425' height='349' src='http://www.youtube.com/embed/#{youtube_id}' frameborder='0' allowfullscreen></iframe>"
+  end
 end
