f06c3995f2978110bc9170e7d7c3d65a29ae0f45
Andrew Braunstein <awbraunstein@gmail.com>
2011-08-14 13:47:42 -0700

    Uploading image from url works

--- app/models/image_slide.rb
+++ app/models/image_slide.rb
@@ -1,5 +1,17 @@
+require 'rubygems'
+require 'imgur'
+
 class ImageSlide < ActiveRecord::Base
-  
   inherits_from :slide
-  
+
+  def upload_file
+    api_key = "1dcdf80fee0df552a19d255e1fd06c6a"
+    imgur = Imgur::API.new(api_key)
+    return imgur.upload_from_url(image_url)
+    # if is_url
+    #   return imgur.upload_from_url(path)["original_image"]
+    # else
+    #   return imgur.upload_file(path)["original_image"]
+    # end
+  end
 end
